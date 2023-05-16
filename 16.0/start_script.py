ce48b72dc82952e8a51166f418458fd083493a51
cfamfl2019 <53031469+cfamfl2019@users.noreply.github.com>
2023-05-16 00:23:12 -0400

    Update start_script.py

--- 16.0/start_script.py
+++ 16.0/start_script.py
@@ -3,4 +3,6 @@
 env['ir.config_parameter'].set_param('web.base.url', 'http://localhost:8069')
 env['ir.config_parameter'].set_param('web.base.url.freeze', 'True')
 env['ir.config_parameter'].set_param('ir_attachment.location', 's3')
+env['ir.config_parameter'].set_param('vsf_cache_invalidation_url', 'http://vsf:3000/cache-invalidate')
+env['ir.config_parameter'].set_param('vsf_cache_invalidation_key', os.getenv('INVALIDATION_KEY'))
 env.cr.commit()
