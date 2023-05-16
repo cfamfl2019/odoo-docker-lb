485fb21a49881a8a003ab46cdb792d6bcf639c82
cfamfl2019 <53031469+cfamfl2019@users.noreply.github.com>
2023-05-16 00:22:16 -0400

    Update startup.sh

--- 16.0/startup.sh
+++ 16.0/startup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+function install_vuestorefront() {
+    if [ ! -d "/mnt/extra-addons/graphql_vuestorefront" ];
+    then 
+        git clone --recurse-submodules --branch 16.0 https://github.com/odoogap/vuestorefront.git /mnt/extra-addons/
+        /entrypoint.sh odoo -d v16_odoo --max-cron-threads 0 --no-http --stop-after-init -i graphql_vuestorefront
+        /entrypoint.sh odoo shell -d v16_odoo --max-cron-threads 0 --stop-after-init --no-http < /start_script.py 
+        exit 0
+    fi
+}
 function install_modules() {
     if [ ! -d "/mnt/extra-addons/session_redis" ];
     then 
@@ -7,9 +16,7 @@ function install_modules() {
         /entrypoint.sh odoo -d v16_odoo --max-cron-threads 0 --no-http --stop-after-init -i session_redis base_attachment_object_storage kwkhtmltopdf_assets attachment_s3  
         /entrypoint.sh odoo shell -d v16_odoo --max-cron-threads 0 --stop-after-init --no-http < /start_script.py 
         exit 0
-    else
-        /entrypoint.sh odoo
     fi
 }
-install_modules
+install_modules && install_vuestorefront && /entrypoint.sh odoo
 exit 0
