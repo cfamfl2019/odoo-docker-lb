87227a9429aa10ca904c95ee3a23dd7dd31fc9e8
cfamfl2019 <53031469+cfamfl2019@users.noreply.github.com>
2023-05-15 23:21:03 -0400

    Update start_script.py

--- 16.0/start_script.py
+++ 16.0/start_script.py
@@ -2,5 +2,5 @@
 
 env['ir.config_parameter'].set_param('web.base.url', 'http://localhost:8069')
 env['ir.config_parameter'].set_param('web.base.url.freeze', 'True')
-
+env['ir.config_parameter'].set_param('ir_attachment.location', 's3')
 env.cr.commit()
